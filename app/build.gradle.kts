0cec42f3bb52d6d58f2083f5027f8638dbe680e7
Developer <dev@multimediaplayer.com>
2026-07-06 15:49:10 +0800

    minSdk 23->26 (POI 需要 minSdk 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.multimediaplayer"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
